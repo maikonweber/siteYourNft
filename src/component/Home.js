725450576d651c2b2363addbd3a90f6fdbfa081f
Maikon <m.carvalho@grouplinknetwork.com>
2022-06-28 17:03:27 -0300

    add new lines

---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+const Install = () => {
+     return (
+          <div> 
+               <h3>
+                    Follow this Link to Install 
+                    <a> https://metamask.io/download.html </a>
+               </h3>
+          </div>
+     );
+};
+
+export default Install;
